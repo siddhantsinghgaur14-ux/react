198bd7ed9e9c5e4cd2bf75b7d641932a1038917a
siddhantsinghgaur14-ux <siddhantsinghgaur14@gmail.com>
2026-08-08 18:31:43 +0530

    Add day 6 React practice

---
+++ day-6-sul-ternary/useRef-rhf/src/main.jsx
@@ -0,0 +1,13 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import From from './component/From.jsx'
+import RHF from './component/RHF.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+    <RHF />
+    // <From />
+ 
+)
